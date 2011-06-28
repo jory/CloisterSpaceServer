eca6a037245197cedaaab51465ee812ed5401e85
Jory Graham <jory.graham@gmail.com>
2011-06-28 17:30:34 -0400

    Initial commit of the bare rails project.

---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+CloisterSpaceServer::Application.config.session_store :cookie_store, :key => '_CloisterSpaceServer_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# CloisterSpaceServer::Application.config.session_store :active_record_store

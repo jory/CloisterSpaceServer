c8f4752c997a73071776de998ece92d458097d43
Jory Graham <jory.graham@gmail.com>
2011-07-05 18:12:23 -0400

    Index action to return all edges as json.

--- app/controllers/edges_controller.rb
+++ app/controllers/edges_controller.rb
@@ -1,11 +1,15 @@
 class EdgesController < ApplicationController
+
+  respond_to :json
   
+  def index
+    @edges = Edge.all
+    respond_with(@edges)
+  end
+
   def show
     @edge = Edge.find(params[:id])
-
-    respond_to do |format|
-      format.json { render :json => @edge }
-    end
+    respond_with(@edge)
   end
 
 end

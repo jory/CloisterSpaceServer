f5915dc43a195354b0bd03875de7269cad6ddd91
Jory Graham <jory.graham@gmail.com>
2011-07-12 13:48:48 -0400

    Added presence validation to all the fields for Edge and Tile.

--- app/models/edge.rb
+++ app/models/edge.rb
@@ -1,5 +1,11 @@
 class Edge < ActiveRecord::Base
 
+  validates :kind, :presence => true
+  validates :road, :presence => true
+  validates :city, :presence => true
+  validates :grassA, :presence => true
+  validates :grassB, :presence => true
+
   def self.find_or_create(kind, road, city, grassA, grassB)
     where(:kind => kind, :road => road, :city => city, :grassA => grassA, :grassB => grassB).first ||
       create(:kind => kind, :road => road, :city => city, :grassA => grassA, :grassB => grassB)
--- app/models/tile.rb
+++ app/models/tile.rb
@@ -1,3 +1,17 @@
 class Tile < ActiveRecord::Base
   has_many :tileInstances
+
+  validates :image, :presence => true
+  validates :count, :presence => true
+  validates :hasTwoCities, :presence => true
+  validates :hasRoadEnd, :presence => true
+  validates :hasPennant, :presence => true
+  validates :isCloister, :presence => true
+  validates :isStart, :presence => true
+  validates :citysFields, :presence => true
+  validates :northEdge, :presence => true
+  validates :southEdge, :presence => true
+  validates :eastEdge, :presence => true
+  validates :westEdge, :presence => true
+
 end

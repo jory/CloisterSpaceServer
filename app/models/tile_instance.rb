89b95ac7d55b543931afd9fd9da62a62ae9854af
Jory Graham <jory.graham@gmail.com>
2011-07-12 16:50:01 -0400

    Added some validations and tests to tile_instance.

--- app/models/tile_instance.rb
+++ app/models/tile_instance.rb
@@ -1,6 +1,19 @@
 class TileInstance < ActiveRecord::Base
+
   belongs_to :tile
   belongs_to :game
+
+  validates :status, :inclusion => { :in => ['current', 'placed', 'discarded'],
+                                     :allow_nil => true }
+  validates :x, :numericality => { :greater_than => -1,
+                                   :less_than => 145,
+                                   :allow_nil => true }
+  validates :y, :numericality => { :greater_than => -1,
+                                   :less_than => 145,
+                                   :allow_nil => true }
+  validates :rotation, :inclusion => { :in => 0..3, :allow_nil => true }
+  validates :tile, :presence => true
+  validates :game, :presence => true
 
   def place(x, y, rotation)
     self.x = x
--- test/unit/tile_instance_test.rb
+++ test/unit/tile_instance_test.rb
@@ -1,8 +1,26 @@
 require 'test_helper'
 
 class TileInstanceTest < ActiveSupport::TestCase
-  # Replace this with your real tests.
-  test "the truth" do
-    assert true
+
+  def setup
+    @game = Game.first
+    @tile = Tile.first
   end
+  
+  test "needs game" do
+    assert !TileInstance.create(:tile => @tile).save
+  end
+
+  test "needs tile" do
+    assert !TileInstance.create(:game => @game).save
+  end
+
+  test "not negative" do
+    instance = TileInstance.create(:tile => @tile, :game => @game)
+    instance.x = -1
+    instance.y = -1
+    instance.rotation = 0
+    assert !instance.save
+  end
+
 end

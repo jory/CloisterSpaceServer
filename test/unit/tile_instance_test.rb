b4d6b890ce8a42f77f40991d204c75cbda1f5ed4
Jory Graham <jory.graham@gmail.com>
2011-07-14 13:07:42 -0400

    Added tests for next().

--- test/unit/tile_instance_test.rb
+++ test/unit/tile_instance_test.rb
@@ -5,12 +5,12 @@ class TileInstanceTest < ActiveSupport::TestCase
   def setup
     @game = Game.first
 
-    @tile = tiles(:start)
+    @startingTile = tiles(:start)
     @otherTile = tiles(:second)
   end
   
   test "needs game" do
-    assert !TileInstance.create(:tile => @tile).save
+    assert !TileInstance.create(:tile => @startingTile).save
   end
 
   test "needs tile" do
@@ -18,33 +18,33 @@ class TileInstanceTest < ActiveSupport::TestCase
   end
 
   test "not negative" do
-    instance = TileInstance.create(:tile => @tile, :game => @game)
+    instance = TileInstance.create(:tile => @startingTile, :game => @game)
     assert !instance.place(-1, -1, 0)
   end
 
   test "no overlapping" do
-    p = TileInstance.create(:tile => @tile, :game => @game)
+    p = TileInstance.create(:tile => @startingTile, :game => @game)
     p.place(0, 0, 0)
 
-    q = TileInstance.create(:tile => @tile, :game => @game)
+    q = TileInstance.create(:tile => @startingTile, :game => @game)
     assert !q.place(0, 0, 0)
   end
 
   test "no double placement" do
-    p = TileInstance.create(:tile => @tile, :game => @game)
+    p = TileInstance.create(:tile => @startingTile, :game => @game)
     p.place(0, 0, 0)
     assert !p.place(1, 1, 1)
   end
 
   test "must set all" do
-    p = TileInstance.create(:tile => @tile, :game => @game)
+    p = TileInstance.create(:tile => @startingTile, :game => @game)
     assert !p.place(nil, 0, 0)
     assert !p.place(0, nil, 0)
     assert !p.place(0, 0, nil)
   end
 
   test "must be adjacent" do
-    p = TileInstance.create(:tile => @tile, :game => @game)
+    p = TileInstance.create(:tile => @startingTile, :game => @game)
     p.place(72, 72, 0)
     
     q = TileInstance.create(:tile => @otherTile, :game => @game)
@@ -53,7 +53,7 @@ class TileInstanceTest < ActiveSupport::TestCase
   end
 
   test "must match edges" do
-    p = TileInstance.create(:tile => @tile, :game => @game)
+    p = TileInstance.create(:tile => @startingTile, :game => @game)
     p.place(72, 72, 0)
 
     q = TileInstance.create(:tile => @otherTile, :game => @game)
@@ -62,7 +62,7 @@ class TileInstanceTest < ActiveSupport::TestCase
   end
 
   test "must match rotated edges" do
-    p = TileInstance.create(:tile => @tile, :game => @game)
+    p = TileInstance.create(:tile => @startingTile, :game => @game)
     p.place(72, 72, 0)
     
     q = TileInstance.create(:tile => @otherTile, :game => @game)
@@ -76,7 +76,45 @@ class TileInstanceTest < ActiveSupport::TestCase
 
     t = TileInstance.create(:tile => @otherTile, :game => @game)
     assert t.place(72, 73, 0), "Couldn't place non-rotated next to both."
+  end
+
+  test "next returns current" do
+    p = TileInstance.create(:tile => @startingTile, :game => @game)
+    p.place(72, 72, 0)
+
+    q = TileInstance.create(:tile => @otherTile, :game => @game)
+    q.status = "current"
+    q.save
+
+    r = TileInstance.create(:tile => tiles(:city4), :game => @game)
     
+    assert q === TileInstance.next(@game)
   end
   
+  test "next returns a tile" do
+    p = TileInstance.create(:tile => @startingTile, :game => @game)
+    p.place(72, 72, 0)
+
+    q = TileInstance.create(:tile => @otherTile, :game => @game)
+
+    r = TileInstance.create(:tile => tiles(:city4), :game => @game)
+    
+    assert TileInstance.next(@game)
+  end
+
+  ##########################################
+  # Unlikely, but definitely needs to be handled at some point.
+  ##########################################
+  # test "shouldn't return unusable tiles" do
+  #   p = TileInstance.create(:tile => @startingTile, :game => @game)
+  #   p.place(72, 72, 0)
+
+  #   q = TileInstance.create(:tile => @otherTile, :game => @game)
+  #   q.place(71, 72, 2)
+    
+  #   r = TileInstance.create(:tile => tiles(:city4), :game => @game)
+  #   assert !TileInstance.next(@game)
+  # end
+  ##########################################
+    
 end

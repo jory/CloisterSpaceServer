bf3661aa52c1ff9ed01d0af67348cc4a9048b5f9
Jory Graham <jory.graham@gmail.com>
2011-07-14 15:07:29 -0400

    Add method shouldn't take nil paramters.

--- app/models/road_feature.rb
+++ app/models/road_feature.rb
@@ -1,4 +1,17 @@
 class RoadFeature < ActiveRecord::Base
-  has_many :road_sections
+  validates :length, :numericality => true
+  validates :numEnds, :numericality => true
+
+  validates :game, :presence => true
+
+  has_many :roadSections
   belongs_to :game
+
+  def add(row, col, edge, num, hasEnd)
+    if row.nil? or col.nil? or edge.nil? or num.nil? or hasEnd.nil?
+      return false
+    end
+
+    return true
+  end
 end
--- test/unit/road_feature_test.rb
+++ test/unit/road_feature_test.rb
@@ -1,8 +1,23 @@
 require 'test_helper'
 
 class RoadFeatureTest < ActiveSupport::TestCase
-  # Replace this with your real tests.
-  test "the truth" do
-    assert true
+
+  def setup
+    @game = Game.first
+  end
+
+  test "needs game" do
+    assert !RoadFeature.create().save
+  end
+
+  test "no nil arguments to add" do
+    rf = RoadFeature.create(:game => @game)
+
+    assert !rf.add(nil, nil, nil, nil, nil)
+    assert !rf.add(nil, 0, :north, 0, false)    
+    assert !rf.add(0, nil, :north, 0, false)    
+    assert !rf.add(0, 0, nil, 0, false)    
+    assert !rf.add(0, 0, :north, nil, false)    
+    assert !rf.add(0, 0, :north, 0, nil)    
   end
 end

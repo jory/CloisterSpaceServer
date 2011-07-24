17b88015e73b6123f6098e059f8e6fcfdb184fff
Jory Graham <jory.graham@gmail.com>
2011-07-24 11:43:13 -0400

    Fleshed out CitySectionTest.

--- test/unit/city_section_test.rb
+++ test/unit/city_section_test.rb
@@ -2,7 +2,34 @@ require 'test_helper'
 
 class CitySectionTest < ActiveSupport::TestCase
 
+  def setup
+    @city = City.create(:game => games(:one))
+  end
+
+  test "invalid row" do
+    assert !CitySection.create(:row =>  -1, :col => 0, :edge => "north", :city => @city).valid?
+    assert !CitySection.create(:row => 145, :col => 0, :edge => "north", :city => @city).valid?
+    assert !CitySection.create(:row => "n", :col => 0, :edge => "north", :city => @city).valid?
+    # assert !CitySection.create(:row => "1", :col => 0, :edge => "north", :city => @city).valid?
+  end
+
+  test "invalid col" do
+    assert !CitySection.create(:col =>  -1, :row => 0, :edge => "north", :city => @city).valid?
+    assert !CitySection.create(:col => 145, :row => 0, :edge => "north", :city => @city).valid?
+    assert !CitySection.create(:col => "n", :row => 0, :edge => "north", :city => @city).valid?
+    # assert !CitySection.create(:col => "1", :row => 0, :edge => "north", :city => @city).valid?
+  end
+
+  test "invalid edge" do
+    assert !CitySection.create(:edge => "n", :row => 0, :col => 0, :city => @city).valid?
+    assert !CitySection.create(:edge =>   1, :row => 0, :col => 0, :city => @city).valid?
+  end
+
   test "needs city" do
-    assert !CitySection.create(:row => 0, :col => 0, :edge => "north").save
+    assert !CitySection.create(:row => 0, :col => 0, :edge => "north").valid?
+  end
+
+  test "valid CitySection" do
+    assert CitySection.create(:row => 0, :col => 0, :edge => "north", :city => @city).valid?
   end
 end

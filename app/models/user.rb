bcc0ec9d7773466adae3d3e8aa23cde0f8bcea2e
Jory Graham <jory.graham@gmail.com>
2011-07-24 15:09:21 -0400

    Fleshed out UserTest.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,6 @@
 class User < ActiveRecord::Base
-  attr_accessible :email
 
-  validates_presence_of :email
-  validates_uniqueness_of :email
+  validates :email, :presence => true, :uniqueness => true
 
   has_many :games
   
--- test/unit/user_test.rb
+++ test/unit/user_test.rb
@@ -1,8 +1,24 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-  # Replace this with your real tests.
-  test "the truth" do
-    assert true
+
+  test "non-unique email is invalid" do
+    User.create(:email => "same@email.com")
+    assert !User.create(:email => "same@email.com").valid?
+  end
+
+  test "authenticate" do
+    User.create(:email => "found@you.com")
+    assert User.authenticate("found@you.com")
+
+    assert_nil(User.authenticate("missing@person.com"))
+  end
+  
+  test "needs email" do
+    assert !User.create().valid?
+  end
+
+  test "valid User" do
+    assert User.create(:email => "some@guy.com").valid?
   end
 end

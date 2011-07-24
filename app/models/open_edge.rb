304b18b4ead0be7a8840dd1175341daa4afea849
Jory Graham <jory.graham@gmail.com>
2011-07-24 14:58:34 -0400

    Added integer qualification for OpenEdges.
    Fleshed out OpenEdgeTest.

--- app/models/open_edge.rb
+++ app/models/open_edge.rb
@@ -1,9 +1,11 @@
 class OpenEdge < ActiveRecord::Base
-  validates :row, :numericality => { :greater_than => -1, :less_than => 145}
-  validates :col, :numericality => { :greater_than => -1, :less_than => 145}
+
+  validates :row, :numericality => { :greater_than => -1, :less_than => 145, :only_integer => true }
+  validates :col, :numericality => { :greater_than => -1, :less_than => 145, :only_integer => true }
   validates :edge, :inclusion => { :in => %w( north south east west ) }
 
   validates :city, :presence => true
   
   belongs_to :city
+
 end
--- test/unit/open_edge_test.rb
+++ test/unit/open_edge_test.rb
@@ -1,8 +1,55 @@
 require 'test_helper'
 
 class OpenEdgeTest < ActiveSupport::TestCase
-  # Replace this with your real tests.
-  test "the truth" do
-    assert true
+
+  def setup
+    @city = City.create(:game => games(:one))
+  end
+
+  test "invalid row" do
+    assert !OpenEdge.create(:col => 0, :edge => "north",
+                            :city => @city).valid?
+
+    assert !OpenEdge.create(:row => -1, :col => 0, :edge => "north",
+                            :city => @city).valid?
+    assert !OpenEdge.create(:row => 145, :col => 0, :edge => "north",
+                            :city => @city).valid?
+    assert !OpenEdge.create(:row => "n", :col => 0, :edge => "north",
+                            :city => @city).valid?
+    assert !OpenEdge.create(:row => 1.1, :col => 0, :edge => "north",
+                            :city => @city).valid?
+  end
+
+  test "invalid col" do
+    assert !OpenEdge.create(:row => 0, :edge => "north",
+                            :city => @city).valid?    
+
+    assert !OpenEdge.create(:row => 0, :col => -1, :edge => "north",
+                            :city => @city).valid?
+    assert !OpenEdge.create(:row => 0, :col => 145, :edge => "north",
+                            :city => @city).valid?
+    assert !OpenEdge.create(:row => 0, :col => "n", :edge => "north",
+                            :city => @city).valid?
+    assert !OpenEdge.create(:row => 0, :col => 1.1, :edge => "north",
+                            :city => @city).valid?
+  end
+
+  test "invalid edge" do
+    assert !OpenEdge.create(:row => 0, :col => 0,
+                            :city => @city).valid?    
+
+    assert !OpenEdge.create(:row => 0, :col => 0, :edge => "n",
+                            :city => @city).valid?
+    assert !OpenEdge.create(:row => 0, :col => 0, :edge => 1,
+                            :city => @city).valid?
+  end
+
+  test "needs city" do
+    assert !OpenEdge.create(:row => 0, :col => 0, :edge => "north").valid?
+  end
+
+  test "valid OpenEdge" do
+    assert OpenEdge.create(:row => 0, :col => 0, :edge => "north",
+                           :city => @city).valid?
   end
 end
